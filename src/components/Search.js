55a0fccb91ae5c4ef5ca3e782318687c759829b1
Kenny Okada <kokada32@gmail.com>
2021-09-12 18:34:22 -0700

    installed axios and executed the request from useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { queryAllByAltText } from '@testing-library/dom';
 
 const Search = () => {
     const [term, setTerm] = useState('');
+
+    useEffect(() => {
+        const search = async () => {
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: "query",
+                    list: "search",
+                    origin: "*",
+                    format: "json",
+                    srsearch: term
+                }
+            });
+        };
+
+        search();
+    }, [term]); //run every time 'term' changes
 
     return (
         <div className="ui form">
