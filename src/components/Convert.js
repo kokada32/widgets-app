3f3db658f67b971709a6ad67af6d4dddc9f87920
Kenny Okada <kokada32@gmail.com>
2021-10-02 18:48:40 -0700

    wired up google translate api

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const Convert = ({language, text}) => {
     useEffect(() => {
-        console.log('new language or text');
+        axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+            params: {
+                q: text,
+                target: language.value,
+                key: process.env.REACT_APP_GOOGLE_TRANSLATE_API
+            }
+        })
     }, [language, text]);
 
     return (
